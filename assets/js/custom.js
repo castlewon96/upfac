854f7a2618e871511c3285e50481dc4d9cbf9a72
정성원 <swjung@itembay.com>
2023-09-05 10:33:30 +0900

    Revert "결제페이지 작업 및 헤더 푸터 작업 반응형X"
    This reverts commit c262f1d45427bd2277d37deb0c5d934e0d6934d7.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,23 +12,3 @@ var swiper = new Swiper(".main_slide", {
         disableOnInteraction: false
     },
   });
-
-// 아코디언 공통
-$(".accordion .accordion_title").on("click", function(){
-    $(this).siblings().stop().slideToggle()
-    $(this).parents('.accordion_list').toggleClass('on');
-    $(this).parents('.accordion_list').siblings().removeClass('on');
-    $(this).parents('.accordion_list').siblings().children(".accordion_toggle").stop().slideUp();
-    $(this).parents().parents().siblings().children().children(".accordion_toggle").stop().slideUp();
-});
-
-// 셀렉스 커스텀 공통
-$(".select_box .select_title").on('click', function(){
-    $(this).parents('.select_box').toggleClass('on')
-});
-
-// 리스트 버튼 한개만 클릭
-$('.list_choice_one li').on('click', function(){
-    $(this).addClass('on')
-    $(this).siblings().removeClass('on')
-})
